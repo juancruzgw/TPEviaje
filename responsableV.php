26ba5b8f98790c81f8b5cebe6a0719052cf80028
juancruzgw <jgeslowski@gmail.com>
2024-04-05 23:45:38 -0300

    cambioss

--- responsableV.php
+++ responsableV.php
@@ -1,4 +1,10 @@
 <?php 
 class Responsable {
-
+    private $numEmpleado;
+    private $numLicencia;
+    private $nombre;
+    private $apellido;
+    public function __construct() {
+        
+    }
 }
---
+++ scripTestV.php
@@ -0,0 +1,4 @@
+<?php
+include_once 'viaje.php';
+include_once 'persona.php';
+include_once 'responsableV.php';
